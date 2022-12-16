9564739fe9079503b4a3824064bb58090ba96fce
IamSithabile <91020386+IamSithabile@users.noreply.github.com>
2022-12-16 10:21:03 +0200

    Exercise 4.4 unit tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,30 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const listWithOneBlog = [
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0,
+  },
+];
+
+const totalLikes = (blogs) => {
+  const likesArray = blogs.map((blog) => blog.likes);
+
+  const likes = likesArray.reduce((acc, cur) => {
+    return acc + cur;
+  }, 0);
+
+  return likes;
+};
+
+totalLikes(listWithOneBlog);
+
 module.exports = {
   dummy,
+  totalLikes,
 };
