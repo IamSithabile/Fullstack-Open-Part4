101cbf30a125980aba6ae973cfab6efae558d536
IamSithabile <91020386+IamSithabile@users.noreply.github.com>
2022-12-16 07:18:35 +0200

    add utils folder with logger. and use it in index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+const { info, error } = require("./utils/logger");
+
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
@@ -17,9 +19,9 @@ const Blog = mongoose.model("Blog", blogSchema);
 const mongoUrl = process.env.MONGODB_URI;
 mongoose.connect(mongoUrl).then((result) => {
   if (result) {
-    console.log("Connected to database");
+    info("Connected to database");
   } else {
-    console.log("Error connectiong to database");
+    error("Error connectiong to database");
   }
 });
 
@@ -29,10 +31,10 @@ app.use(express.json());
 app.get("/api/blogs", (request, response) => {
   Blog.find({}).then((blogs) => {
     if (blogs.length === 0) {
-      console.log("There are currently no blogs in database");
+      info("There are currently no blogs in database");
     }
     if (blogs.length > 0) {
-      console.log("Here are the blogs:", blogs);
+      info("Here are the blogs:", blogs);
     }
     response.json(blogs);
   });
@@ -42,12 +44,12 @@ app.post("/api/blogs", (request, response) => {
   const blog = new Blog(request.body);
 
   blog.save().then((result) => {
-    console.log("blog saved", result);
+    info("blog saved", result);
     response.status(201).json(result);
   });
 });
 
 const PORT = 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  info(`Server running on port ${PORT}`);
 });
